e8947a2fb0a93c1a55e59d54e0f78ad8963d9c94
Vallen217 <EidolonVallen@gmail.com>
2024-12-07 01:06:57 -0600

    Removed deprecated file

--- shipwright_build.lua
+++
@@ -1,5 +0,0 @@
--- local colorscheme = require("lua.lush_theme.eidolon")
-local lushwright = require("shipwright.transform.lush")
-run(require("lua.lush_theme.eidolon"),
-	lushwright.to_lua,
-	{ patchwrite, "lua/lush_theme/eidolon.lua", "-- PATCH_OPEN", "-- PATCH_CLOSE" })
